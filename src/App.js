7acfbaf40196705f602b36b79dfef72f0e0ccd9f
Pen <dimapen57@gmail.com>
2023-06-04 17:35:31 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,249 @@
+function App() {
+  return (
+    <div className="wrapper">
+      <header className="header">
+        <div className="headerLeft">
+          <img width={40} height={40} src="/img/logo.png" />
+          <div className="headerInfo">
+            <h3>React Sneakers</h3>
+            <p>Shop of the best shoes</p>
+          </div>
+        </div>
+        <div>
+          <ul className="headerRight">
+            <li className="headerRightLi">
+              <img width={18} height={18} src="/img/cart.svg" />
+              <span>1205 pounds</span>
+            </li>
+            <li>
+              <img width={20} height={20} src="/img/user.svg" />
+            </li>
+          </ul>
+        </div>
+      </header>
+      <div className="content">
+        <h1>All shoes</h1>
+        <div className="sneakers">
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="/img/sneakers/1.jpg"
+              alt="Sneakers"
+            />
+            <h5>Shoes for proper man, son of a bitch</h5>
+            <div className="cardBottom">
+              <div className="cardBottom_descript">
+                <span>Price:</span>
+                <b>12 999 pound</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg"></img>
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="/img/sneakers/2.jpg"
+              alt="Sneakers"
+            />
+            <h5>Shoes for proper man, son of a bitch</h5>
+            <div className="cardBottom">
+              <div className="cardBottom_descript">
+                <span>Price:</span>
+                <b>12 999 pound</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg"></img>
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="/img/sneakers/3.jpg"
+              alt="Sneakers"
+            />
+            <h5>Shoes for proper man, son of a bitch</h5>
+            <div className="cardBottom">
+              <div className="cardBottom_descript">
+                <span>Price:</span>
+                <b>12 999 pound</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg"></img>
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="/img/sneakers/4.jpg"
+              alt="Sneakers"
+            />
+            <h5>Shoes for proper man, son of a bitch</h5>
+            <div className="cardBottom">
+              <div className="cardBottom_descript">
+                <span>Price:</span>
+                <b>12 999 pound</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg"></img>
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="/img/sneakers/5.jpg"
+              alt="Sneakers"
+            />
+            <h5>Shoes for proper man, son of a bitch</h5>
+            <div className="cardBottom">
+              <div className="cardBottom_descript">
+                <span>Price:</span>
+                <b>12 999 pound</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg"></img>
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="/img/sneakers/6.jpg"
+              alt="Sneakers"
+            />
+            <h5>Shoes for proper man, son of a bitch</h5>
+            <div className="cardBottom">
+              <div className="cardBottom_descript">
+                <span>Price:</span>
+                <b>12 999 pound</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg"></img>
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="/img/sneakers/7.jpg"
+              alt="Sneakers"
+            />
+            <h5>Shoes for proper man, son of a bitch</h5>
+            <div className="cardBottom">
+              <div className="cardBottom_descript">
+                <span>Price:</span>
+                <b>12 999 pound</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg"></img>
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="/img/sneakers/8.jpg"
+              alt="Sneakers"
+            />
+            <h5>Shoes for proper man, son of a bitch</h5>
+            <div className="cardBottom">
+              <div className="cardBottom_descript">
+                <span>Price:</span>
+                <b>12 999 pound</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg"></img>
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="/img/sneakers/9.jpg"
+              alt="Sneakers"
+            />
+            <h5>Shoes for proper man, son of a bitch</h5>
+            <div className="cardBottom">
+              <div className="cardBottom_descript">
+                <span>Price:</span>
+                <b>12 999 pound</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg"></img>
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="/img/sneakers/10.jpg"
+              alt="Sneakers"
+            />
+            <h5>Shoes for proper man, son of a bitch</h5>
+            <div className="cardBottom">
+              <div className="cardBottom_descript">
+                <span>Price:</span>
+                <b>12 999 pound</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg"></img>
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="/img/sneakers/11.jpg"
+              alt="Sneakers"
+            />
+            <h5>Shoes for proper man, son of a bitch</h5>
+            <div className="cardBottom">
+              <div className="cardBottom_descript">
+                <span>Price:</span>
+                <b>12 999 pound</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg"></img>
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="/img/sneakers/12.jpg"
+              alt="Sneakers"
+            />
+            <h5>Shoes for proper man, son of a bitch</h5>
+            <div className="cardBottom">
+              <div className="cardBottom_descript">
+                <span>Price:</span>
+                <b>12 999 pound</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg"></img>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
